2a890b8ec48b3d49589304b4530aa423987f52c5
Adrien GOGOIS <ad.gogois@gmail.com>
2022-06-09 16:26:26 +0200

    Refactoring to respect signle responsibility method

--- src/main/java/org/example/FizzBuzz.java
+++ src/main/java/org/example/FizzBuzz.java
@@ -1,22 +1,29 @@
 package org.example;
 
 public class FizzBuzz {
-
+    private int numberToConvert;
     public String fizzBuzzConvertor(int numberToConvert) {
 
+        this.numberToConvert = numberToConvert;
 
-            if (numberToConvert % 15 == 0) {
-                return "FizzBuzz";
-            }
+        if (isMultipleOf15()) { return "FizzBuzz"; }
 
-            if (numberToConvert % 3 == 0) {
-                return "Fizz";
-            }
+        if (isMultipleOf5()) { return "Buzz"; }
 
-            if (numberToConvert % 5 == 0) {
-                return "Buzz";
-            }
+        if (isMultipleOf3()) { return "Fizz"; }
 
         return Integer.toString(numberToConvert);
+    }
+
+    private boolean isMultipleOf15() {
+        return numberToConvert % 15 == 0;
+    }
+
+    private boolean isMultipleOf5() {
+        return numberToConvert % 5 == 0;
+    }
+
+    private boolean isMultipleOf3() {
+        return numberToConvert % 3 == 0;
     }
 }
